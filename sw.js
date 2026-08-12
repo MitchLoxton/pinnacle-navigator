a16cdbc3edf2e53bd6c81dc67d932d89683d37f2
MitchLoxton <mitchloxton@gmail.com>
2026-08-12 19:55:34 +0800

    Purge stale private Navigator bundles on v48 activation

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('pn-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>(k.startsWith('pn-shell-')&&k!==CACHE)||k.startsWith('pn-private-bundle-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',event=>{
   const req=event.request;
