bf109c4d2f9fa6bdcb79562e9406fbbc9e2e2c07
MitchLoxton <mitchloxton@gmail.com>
2026-08-26 20:07:53 +0800

    Make All 21 stats always visible
    Replace collapsible race cards with a flat always-visible All 21 scan board and bump the racing PWA cache.

--- racing/sw.js
+++ racing/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mitchell-racing-shell-v8';
+const CACHE='mitchell-racing-shell-v9';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./apple-touch-icon.png','./current.json','./stats.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
