5dbad9dd9ba15b792ac18fc1f7bc08d7868852b3
MitchLoxton <mitchloxton@gmail.com>
2026-08-26 08:21:15 +0800

    Refresh racing cache for active expansion

--- racing/sw.js
+++ racing/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mitchell-racing-shell-v6';
+const CACHE='mitchell-racing-shell-v7';
 const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./apple-touch-icon.png','./current.json','./stats.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
