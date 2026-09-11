8dd757df1a76b5863b2a717b7db0bfea05b284df
MitchLoxton <mitchloxton@gmail.com>
2026-09-11 08:03:24 +0800

    Release Navigator v53.65 resilient shell cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const BUILD='5371';
-const VERSION='v53.64';
-const SHELL='pn-shell-stable-v5371';
+const BUILD='5372';
+const VERSION='v53.65';
+const SHELL='pn-shell-stable-v5372';
 const LOCAL=['./','./index.html','./manifest.webmanifest','./icon.svg','./v51.css','./v51.js','./fabrication.js','./stability-core.js','./runtime-late.js','./workshop-ui-fast.js','./navigator-nova.css','./morning-pack.js','./miter-48-template.html','./repair.html'];
 const SCOPE_PATH=new URL(self.registration.scope).pathname.replace(/\/?$/,'/');
 function canonical(url){return new Request(new URL(url.pathname,self.location.origin).href);}
